ad03bd5c8410f999443fd78b89cb9b2579cedfff
OZYMAYNDIAS <kalkidanh89@gmail.com>
2023-07-06 19:11:03 +0300

    Product catalog system update
    Front-end master uploaded.

---
+++ frontend-master/src/utils/Data.js
@@ -0,0 +1,38 @@
+const services =[
+    {
+        "image":"images/service.png",
+        "title":"Free Shipping",
+        "tageline":"From All orders over $5"
+         
+    },
+    {
+        image:"images/service-02.png",
+        title:"Daily Suprise Offers",
+        tageline:"Save Upto 255 off"
+         
+    },
+    {
+        image:"images/service-03.png",
+        title:"Free Shipping",
+        tageline:"From All orders over $5"
+         
+    },
+    {
+        image:"images/service-04.png",
+        title:"Free Shipping",
+        tageline:"From All orders over $5"
+         
+    },
+    {
+        image:"images/service-05.png",
+        title:"Free Shipping",
+        tageline:"From All orders over $5"
+         
+    },
+    {
+        image:"images/service-04.png",
+        title:"Free Shipping",
+        tageline:"From All orders over $5"
+         
+    }
+];
