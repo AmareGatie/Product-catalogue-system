0f050680ad18099e0411db2c005df4fb13c35eb3
AmareGatie <113662551+AmareGatie@users.noreply.github.com>
2023-07-09 13:12:37 +0300

    Update uploadImage.js

--- Backend-master/middlewares/uploadImage.js
+++ Backend-master/middlewares/uploadImage.js
@@ -34,11 +34,12 @@ const productImgResize = async (req, res, next) => {
         .resize(300, 300)
         .toFormat("jpeg")
         .jpeg({ quality: 90 })
-        .toFile(`../public/images/products${file.filename}`);
-      fs.unlinkSync(`../public/images/products${file.filename}`);
+        .toFile(`../public/images${file.filename}`);
+      fs.unlinkSync(`../public/images${file.filename}`);
     })
   );
   next();
+  sharp.cache(false);
 };
 
 const blogImgResize = async (req, res, next) => {
